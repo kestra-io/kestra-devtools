1025bdd52de6ccf7e375865da0b7f78f320c154f
Roman Acevedo <roman.acevedo62@gmail.com>
2025-09-19 16:20:39 +0200

    fix: bin 2

--- src/kestra-devtools.ts
+++ src/kestra-devtools.ts
@@ -1,11 +1,9 @@
 // If executed directly, run main()
 import { main } from "./cli";
 
-if (import.meta.url === `file://${process.argv[1]}`) {
-    main()
-        .then((code) => process.exit(code))
-        .catch((err) => {
-            console.error(err);
-            process.exit(1);
-        });
-}
+main()
+  .then((code) => process.exit(code))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
