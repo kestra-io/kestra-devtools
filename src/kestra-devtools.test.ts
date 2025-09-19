e62ea3141f3583fb90520578a9e25d8c30c7d7d0
Roman Acevedo <roman.acevedo62@gmail.com>
2025-09-19 13:14:26 +0200

    migrate code

---
+++ src/kestra-devtools.test.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect, vi } from "vitest";
+import { main } from "./kestra-devtools";
+
+describe("cli tests", () => {
+    it("prints hello with default", async () => {
+      const logs: string[] = [];
+        const spy = vi.spyOn(console, "log").mockImplementation(l => logs.push(l));
+        await main(["node", "cli"]);
+        expect(spy).toHaveBeenCalled();
+        expect(logs.reduce((a, b) => a+'___'+b)).contain("Show help")
+        spy.mockRestore();
+    });
+});
