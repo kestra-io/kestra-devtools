d61ee958c68289c174e85540e84694994188b426
Roman Acevedo <roman.acevedo62@gmail.com>
2025-10-27 19:17:04 +0100

    add missing

--- dist/types/utilities/working-dir.d.ts
+++ dist/types/utilities/working-dir.d.ts
@@ -1,6 +1,12 @@
 export type WorkingDir = string;
+export declare function getDefaultWorkingDir(): WorkingDir;
 /**
  * helper to handle working dir passed in CLI
  * @param workingDir by default the repository root
  */
-export declare function getWorkingDir(workingDir?: string): WorkingDir;
+export declare function validateWorkingDir(workingDir?: string): WorkingDir;
+/**
+ * helper to know if the CLI is run in Kestra OSS repo or EE, or not
+ * @param workingDir
+ */
+export declare function findIfRanInKestraRepo(workingDir: WorkingDir): Promise<"kestra-oss" | "kestra-ee" | "unknown">;
